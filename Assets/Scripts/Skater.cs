ada4b0ed0d6e071bdf8363b09b32dca88de31977
spentak <mprice@verisagellc.com>
2015-03-13 21:31:41 -0600

    Added push and ollie animations

---
+++ Assets/Scripts/Skater.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skater : MonoBehaviour {
+
+	Animator animator;
+	bool jumping;
+
+	void Awake() {
+		DebugUtil.Assert(gameObject.GetComponent<Rigidbody2D>() != null);
+		DebugUtil.Assert(gameObject.GetComponent<Animator>() != null);
+	}
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonDown("Fire1") && jumping == false)
+		{
+			jumping = true;
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+			animator.SetTrigger("ollie");
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Ground")
+			jumping = false;
+	}
+}
